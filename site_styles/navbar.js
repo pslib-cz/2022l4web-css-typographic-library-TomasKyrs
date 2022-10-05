122f1abcd376db697a64c70094f840f08f31c865
Tomáš Kyrš <tomaskyrs@seznam.cz>
2022-10-05 15:48:11 +0200

    html + css + javascript used

---
+++ site_styles/navbar.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "6px 3px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "15px 8px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
