63307a373e267e8533fd9891208d4700d42467be
Tomáš Kyrš <tomaskyrs@seznam.cz>
2022-10-10 13:22:06 +0200

    html + css + javascript

--- docs/navbar.js
+++ docs/navbar.js
@@ -2,10 +2,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "6px 3px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.padding = "4px 2px";
+    document.getElementById("navbar").style.backgroundColor = "#376847";
   } else {
-    document.getElementById("navbar").style.padding = "15px 8px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "10px 5px";
+    document.getElementById("navbar").style.backgroundColor = "white";
   }
 }
