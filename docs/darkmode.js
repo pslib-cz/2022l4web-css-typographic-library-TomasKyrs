17cff7b7db241c6d50f590f1de11cb79e18258b3
Tomáš Kyrš <tomaskyrs@seznam.cz>
2022-10-17 13:19:38 +0200

    html + css + js + svg

--- docs/darkmode.js
+++ docs/darkmode.js
@@ -1,25 +1,25 @@
 function darkmode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
-  if (localStorage.getItem("theme") === "dark") {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
+if (localStorage.getItem("theme") === "dark") {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var checkbox = document.querySelector('input[type="checkbox"]');
-  
-    checkbox.addEventListener('change', function () {
-      if (checkbox.checked) {
-        var element = document.body;
-        element.classList.toggle("dark-mode");
-        localStorage.setItem("theme", "dark");
-      } else {
-        var element = document.body;
-        element.classList.toggle("dark-mode");
-        localStorage.setItem("theme", "light");
-      }
-    });
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+      localStorage.setItem("theme", "dark");
+    } else {
+      var element = document.body;
+      element.classList.toggle("dark-mode");
+      localStorage.setItem("theme", "light");
+    }
   });
+});
--- docs/navbar.js
+++ docs/navbar.js
@@ -1,20 +1,20 @@
 window.onscroll = function() {
   scrollFunction();
-  OnScrollWindow();
+  //OnScrollWindow();
 };
 
 function scrollFunction() {
   if (document.body.scrollTop > 580 || document.documentElement.scrollTop > 580) {
     document.getElementById("navbar").style.padding = "4px 2px";
-    document.getElementById("navbar").style.backgroundColor = "#376847";
+    document.getElementById("navbar").classList.add('sticky');
   } else {
     document.getElementById("navbar").style.padding = "10px 5px";
-    document.getElementById("navbar").style.backgroundColor = "white";
+    document.getElementById("navbar").classList.remove('sticky');
   }
 }
 
 
-function OnScrollWindow() {
+/*function OnScrollWindow() {
   if (document.body.scrollTop > 570 || document.documentElement.scrollTop > 570) {
 var elements = document.getElementsByClassName("navbar-menu__link");
 elements[0].className = "navbar-menu__link--override";
@@ -26,4 +26,4 @@ elements[0].className = "navbar-menu__link--override";
 for (var i = 0; i < elements.length; i++){
     elements[i].className = "navbar-menu__link navbar-menu__link--override";
 }
-}
+}*/
